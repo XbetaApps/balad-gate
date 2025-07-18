04425c5f19d9c923967db43407cf141778268084
Ameed-salameh <ameed.salame123@gmail.com>
2025-07-18 16:49:48 +0300

    update

---
+++ push-all.bat
@@ -0,0 +1,20 @@
+@echo off
+echo push origin/main...
+git push origin main
+
+if %ERRORLEVEL% NEQ 0 (
+    echo error push  origin/main
+    exit /b %ERRORLEVEL%
+)
+
+echo.
+echo push xbeta/ameed...
+git push xbeta ameed
+
+if %ERRORLEVEL% NEQ 0 (
+    echo   error push  xbeta/ameed
+    exit /b %ERRORLEVEL%
+)
+
+echo.
+echo done all
