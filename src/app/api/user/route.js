0fc4459c0f2e8dd9835836aa9b6c59a6787efd98
XBeta <xbetaapps@gmail.com>
2025-07-19 12:56:58 +0300

    Fix: Update authOptions imports to use @/ alias

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { PrismaClient } from '@prisma/client';
-import { authOptions } from '../auth/[...nextauth]/route';
+import authOptions from '@/lib/authOptions';
 
 const prisma = new PrismaClient();
 
