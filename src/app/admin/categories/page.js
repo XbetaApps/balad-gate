5e248e3c675ac5d8a5c29376075ded65ad538141
XBeta <xbetaapps@gmail.com>
2025-08-27 13:09:50 +0300

    Fix table cell closing tags and update admin categories

--- src/app/admin/categories/page.js
+++ src/app/admin/categories/page.js
@@ -304,18 +304,18 @@ const AdminCategories = () => {
               <TableRow>
                 <TableCell>الاسم</TableCell>
                 <TableCell>الرابط</TableCell>
-                <TableCell>الوصف</td>
-                <TableCell>الحالة</td>
-                <TableCell>الترتيب</td>
-                <TableCell>الإجراءات</td>
+                <TableCell>الوصف</TableCell>
+                <TableCell>الحالة</TableCell>
+                <TableCell>الترتيب</TableCell>
+                <TableCell>الإجراءات</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {categories.map((category) => (
                 <TableRow key={category.id}>
-                  <TableCell>{category.name}</td>
-                  <TableCell>{category.slug}</td>
-                  <TableCell>{category.description || '-'}</td>
+                  <TableCell>{category.name}</TableCell>
+                  <TableCell>{category.slug}</TableCell>
+                  <TableCell>{category.description || '-'}</TableCell>
                   <TableCell>
                     <FormControlLabel
                       control={
